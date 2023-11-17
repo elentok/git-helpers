dbd831a590c5e86d0f34a6d65adcb05c0da1ae6c
David Elentok <3david@gmail.com>
2023-11-17 22:32:52 +0200

    Cleanup

--- src/lib/git.ts
+++ src/lib/git.ts
@@ -34,15 +34,17 @@ export function getRevCount(repo: Repo, fromRef: string, toRef: string): number 
 }
 
 export function hasUncommitedChanges(repo: Repo): boolean {
-  return (
-    git(repo, ["status", "--porcelain=v1"])
-      .stdout.split("\n")
-      .filter((l) => !l.startsWith("?? ")).length > 0
-  )
+  const output = git(repo, ["status", "--porcelain=v1"]).stdout
+  if (output.length === 0) return false
+
+  const lines = output.split("\n").filter((l) => l.length > 0 && !l.startsWith("?? "))
+  return lines.length > 0
 }
 
 export function hasUntrackedFiles(repo: Repo): boolean {
-  const lines = git(repo, ["status", "--porcelain=v1"]).stdout.split("\n")
+  const output = git(repo, ["status", "--porcelain=v1"]).stdout
+  if (output.length === 0) return false
 
+  const lines = output.split("\n")
   return lines.find((l) => l.startsWith("?? ")) != null
 }
